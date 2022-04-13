46bc9826a2d894632d2256ab676545e0428e4b26
Rahul Yadav <imdotrahul@gmail.com>
2022-04-13 23:26:39 +0530

    Initial Commit

---
+++ twitterclone/twittercloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  twittercloneApp.swift
+//  twitterclone
+//
+//  Created by Rahul Yadav on 13/04/22.
+//
+
+import SwiftUI
+
+@main
+struct twittercloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
